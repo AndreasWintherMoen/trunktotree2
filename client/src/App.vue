b38bf07d6a7f904aadee60723f67d16b04c7a080
AndreasWintherMoen <andreas@winthermoen.no>
2021-11-08 21:48:40 +0100

    Add splash screen

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
-    <div class="flex content-center flex-grow h-12 dnb-green">
+    <splash-screen :isLoading="isLoading" />
+    <div v-if="!isLoading">
+      <div class="flex content-center flex-grow h-12 dnb-green" />
       <div
         id="nav"
         class="
@@ -14,22 +16,65 @@
       >
         <h3 class="font-bold text-lg">EcoreHealth+</h3>
       </div>
-    </div>
-    <router-view class="custom-min-height" />
-    <div class="h-16"></div>
-    <div id="nav"  class="fixed bottom-0 min-w-full dnb-green h-16 flex flex-row items-center justify-between px-24">
-        <router-link to="/" class="rounded-full bg-opacity-50 bg-green-400 p-1.5"><img class="nav-img" :src="`${publicPath}home-outline.png`" alt="Home"></router-link>
-        <router-link to="/challenges" class="rounded-full bg-opacity-50 bg-green-400 p-1.5"><img class="nav-img" :src="`${publicPath}calendar-check-outline.png`" alt="Challenges"></router-link>
-        <router-link to="/map" class="rounded-full bg-opacity-50 bg-green-400 p-1.5"><img class="nav-img" :src="`${publicPath}map-outline.png`" alt="Map"></router-link>
+      <router-view class="custom-min-height" />
+      <div class="h-16"></div>
+      <div
+        id="nav"
+        class="
+          fixed
+          bottom-0
+          min-w-full
+          dnb-green
+          h-16
+          flex flex-row
+          items-center
+          justify-between
+          px-24
+        "
+      >
+        <router-link
+          to="/"
+          class="rounded-full bg-opacity-50 bg-green-400 p-1.5"
+          ><img
+            class="nav-img"
+            :src="`${publicPath}home-outline.png`"
+            alt="Home"
+        /></router-link>
+        <router-link
+          to="/challenges"
+          class="rounded-full bg-opacity-50 bg-green-400 p-1.5"
+          ><img
+            class="nav-img"
+            :src="`${publicPath}calendar-check-outline.png`"
+            alt="Challenges"
+        /></router-link>
+        <router-link
+          to="/map"
+          class="rounded-full bg-opacity-50 bg-green-400 p-1.5"
+          ><img class="nav-img" :src="`${publicPath}map-outline.png`" alt="Map"
+        /></router-link>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import SplashScreen from "./components/pages/SplashScreen.vue";
 export default {
+  components: { SplashScreen },
   setup() {
-    const publicPath = process.env.BASE_URL
-    return {publicPath}
+    const publicPath = process.env.BASE_URL;
+    return { publicPath };
+  },
+  data: function () {
+    return {
+      isLoading: true,
+    };
+  },
+  mounted() {
+    setTimeout(() => {
+      this.isLoading = false;
+    }, 2500);
   },
 };
 </script>
@@ -64,11 +109,11 @@ body {
   color: #22b171;
 }
 
-.custom-min-height{
+.custom-min-height {
   min-height: calc(100% + 10rem) !important;
 }
 
-.nav-img{
+.nav-img {
   width: 32px;
   margin: 0 auto;
 }
---
+++ client/src/components/pages/SplashScreen.vue
@@ -0,0 +1,60 @@
+<template>
+  <div :class="{ loader: true, fadeout: !isLoading }">
+    <!-- <img
+      class="w-10/12 mb-8"
+      src="../../assets/images/LogoFull.png"
+      alt="Logo"
+    /> -->
+
+    <img
+      class="w-10/12 mt-16"
+      src="../../assets/images/LogoText.png"
+      alt="Logo"
+    />
+
+    <img class="w-6/12" src="../../assets/images/LogoTree.png" alt="Logo" />
+
+    <p class="loadingText mb-16">Loading ...</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LoadingScreen",
+  props: ["isLoading"],
+};
+</script>
+
+<style>
+.loader {
+  background-color: #81d4bd;
+  bottom: 0;
+  color: white;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+  font-size: 32px;
+  left: 0;
+  overflow: hidden;
+  position: fixed;
+  right: 0;
+  top: 0;
+  height: 100vh;
+}
+
+.fadeout {
+  animation: fadeout 2s forwards;
+}
+
+.loadingText {
+  color: rgb(37, 109, 244);
+}
+
+@keyframes fadeout {
+  to {
+    opacity: 0;
+    visibility: hidden;
+  }
+}
+</style>
