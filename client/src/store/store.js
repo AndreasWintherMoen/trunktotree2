a1f2a0353c7d92d27557309423caa0c293211a4f
kpaubert <kpaubert@gmail.com>
2021-11-10 21:04:04 +0100

    dumb code nice result

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -3,12 +3,12 @@ import generateSpendingBars from "../../util/generateSpendingBars";
 
 export const store = createStore({
   state: {
-    monthSelected: new Date().toLocaleString("default", { month: "long" }),
+    monthSelected: (new Date().toLocaleString("default", { month: "long" })).charAt(0).toUpperCase() + (new Date().toLocaleString("default", { month: "long" })).slice(1),
     spendingBars: generateSpendingBars(),
   },
   mutations: {
     setMonth(state, month) {
-      state.monthSelected = month;
+      state.monthSelected = month.charAt(0).toUpperCase() + month.slice(1);
     },
   },
 });
